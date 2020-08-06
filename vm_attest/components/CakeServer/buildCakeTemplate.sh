18d7d61362e07746d7f9622c36c7b6394b66be1c
Michael Neises <neisesmichael@gmail.com>
2020-08-05 22:45:42 -0500

    update cakeml usage with Grant's refactoring
    I had been operating with two forks at the same time: the old private one and this one. I made so much progress on the private one that I'm now essentially re-forking it. I wish I could include the git history from the private repo, but alas.

--- vm_attest/components/CakeServer/buildCakeTemplate.sh
+++ vm_attest/components/CakeServer/buildCakeTemplate.sh
@@ -2,22 +2,35 @@
 echo "append the cakeml files for the cakeml_am" &&
 (rm cake_server.cml || true) &&
 touch cake_server.cml &&
+
+# util src
 cat cakeml_am/util/ByteString.sml >> cake_server.cml && 
-cat cakeml_am/crypto/CryptoFFI.sml >> cake_server.cml && 
-cat cakeml_am/crypto/Aes256.sml >> cake_server.cml &&
-cat cakeml_am/crypto/Random.sml >> cake_server.cml && 
-cat cakeml_am/sockets/SocketFFI.sml >> cake_server.cml && 
 cat cakeml_am/util/CoqDefaults.sml >> cake_server.cml && 
-cat cakeml_am/interp/CoplandLang.sml >> cake_server.cml && 
+cat cakeml_am/JSON/Json.sml >> cake_server.cml && 
+
+# copland src
+cat cakeml_am/copland/Instr.sml >> cake_server.cml && 
+cat cakeml_am/copland/AM.sml >> cake_server.cml && 
+cat cakeml_am/copland/VM.sml >> cake_server.cml && 
+cat cakeml_am/copland/json/CoplandToJson.sml >> cake_server.cml && 
+cat cakeml_am/copland/json/JsonToCopland.sml >> cake_server.cml && 
+
+# crypto src
+cat cakeml_am/system/crypto/CryptoFFI.sml >> cake_server.cml && 
+
+# socket src
+cat cakeml_am/system/sockets/SocketFFI.sml >> cake_server.cml && 
+
+# dataport src
+cat cakeml_am/system/dataports/DataportFFI.sml >> cake_server.cml && 
+
+# server_am src
 cat cakeml_am/am/Measurements.sml >> cake_server.cml && 
 cat cakeml_am/am/CommTypes.sml >> cake_server.cml && 
-cat cakeml_am/interp/Instr.sml >> cake_server.cml && 
-cat cakeml_am/JSON/Json.sml >> cake_server.cml && 
-cat cakeml_am/JSON/CoplandToJson.sml >> cake_server.cml && 
-cat cakeml_am/JSON/JsonToCopland.sml >> cake_server.cml && 
 cat cakeml_am/am/CommUtil.sml >> cake_server.cml && 
-cat cakeml_am/interp/Eval.sml >> cake_server.cml && 
-cat cakeml_am/interp/VM.sml >> cake_server.cml && 
+cat cakeml_am/am/ServerAm.sml >> cake_server.cml && 
+
+# plat_am src
 cat destination.cml >> cake_server.cml && 
 
 echo "compile the dest cakeml" &&
