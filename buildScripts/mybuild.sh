8104a79a49bd455ce5dce49be13403181e2c6542
Michael Neises <neisesmichael@gmail.com>
2020-10-12 21:49:14 -0500

    add vm_intro app from vm_introspection

--- buildScripts/mybuild.sh
+++ buildScripts/mybuild.sh
@@ -3,33 +3,33 @@
 echo "====================================" &&
 echo "Grabbing the latest vm_attest..." &&
 
-if rm -rf ../projects/vm/apps/vm_attest ; then 
+if rm -rf ../projects/vm/apps/vm_intro ; then 
     true
 else
-    echo "The vm_attest app wasn't there."
+    echo "The vm_intro app wasn't there."
 fi &&
 
 echo "..." &&
-cp -r ../../vm_attest ../projects/vm/apps/ &&
+cp -r ../../vm_intro ../projects/vm/apps/ &&
 echo "vm_attest updated." &&
 echo "Trying an initial build..." &&
 echo "====================================" &&
 
 
-#if ../init-build.sh -DCAMKES_VM_APP=vm_attest -DPLATFORM=exynos5422 -DAARCH32=1; then
-if ../init-build.sh -DCAMKES_VM_APP=vm_attest -DPLATFORM=exynos5422 -DAARCH32=1 -DCROSS_COMPILER_PREFIX=arm-linux-gnueabihf-; then
-    true
-else
-    echo "====================================" &&
-    echo "Initial build failed." &&
-    echo "Building cake and moving libs..." &&
-    echo "====================================" &&
-    cd ../../vm_attest/components/CakeServer &&
-    ./buildCakeDocker.sh &&
-    cd - &&
-    #../init-build.sh -DCAMKES_VM_APP=vm_attest -DPLATFORM=exynos5422 -DAARCH32=1
-    ../init-build.sh -DCAMKES_VM_APP=vm_attest -DPLATFORM=exynos5422 -DAARCH32=1 -DCROSS_COMPILER_PREFIX=arm-linux-gnueabihf-
-fi &&
+../init-build.sh -DCAMKES_VM_APP=vm_intro -DPLATFORM=qemu-arm-virt &&
+#if ../init-build.sh -DCAMKES_VM_APP=vm_intro -DPLATFORM=qemu-arm-virt; then
+#    true
+#else
+#    echo "====================================" &&
+#    echo "Initial build failed." &&
+#    echo "Building cake and moving libs..." &&
+#    echo "====================================" &&
+#    cd ../../vm_attest/components/CakeServer &&
+#    ./buildCakeDocker.sh &&
+#    cd - &&
+#    #../init-build.sh -DCAMKES_VM_APP=vm_attest -DPLATFORM=exynos5422 -DAARCH32=1
+#    ../init-build.sh -DCAMKES_VM_APP=vm_intro -DPLATFORM=qemu-arm-virt
+#fi &&
 echo "====================================" &&
 echo "Build succeeded." &&
 echo "Calling ninja..." &&
