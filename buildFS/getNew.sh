8d5fbb713798ebc787ebf93a178affa4d0484906
Michael Neises <neisesmichael@gmail.com>
2021-06-12 19:48:58 -0500

    remove unnecessary scripts

--- buildFS/getNew.sh
+++
@@ -1,9 +0,0 @@
-vmdir=camkesvm`date +"%B%d"`
-fsdir=fs`date +"%B%d"`
-mkdir ${fsdir} &&
-cd ${fsdir} &&
-cp /home/michael/Documents/projects/sel4/camkes_vm_attest/${vmdir}/projects/camkes-vm-images/exynos5422/rootfs.cpio.gz . &&
-gunzip rootfs.cpio.gz &&
-fakeroot cpio -id --no-preserve-owner --preserve-modification-time < rootfs.cpio &&
-rm rootfs.cpio
-
